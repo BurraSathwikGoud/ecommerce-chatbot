6055e250ac16e37759ed4dc2e54e3f34db9c06d6
Sathwik Goud Burra <burrasathwik92@gmail.com>
2025-07-26 13:14:50 +0530

    Update users.py

--- backend/users.py
+++ backend/users.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify
+from app.models.user import User
+from app.models.db import db
 
 users_bp = Blueprint('users', __name__)
 
 @users_bp.route('/', methods=['GET'])
 def get_users():
-    return jsonify({"message": "User route is working!"})
-
-
-
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users])
